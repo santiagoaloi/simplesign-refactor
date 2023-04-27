7ac952e1e0a2d9212d1adca2da42436a8eeeacce
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 01:59:36 +0200

    chore(templates):  table data in separate file

---
+++ src/pages/template/data/table-data.js
@@ -0,0 +1,102 @@
+export const headers = [
+  {
+    title: 'Document',
+    align: 'start',
+    key: 'document',
+    width: '320'
+  },
+  { title: 'Shared With', align: 'center', key: 'shared', width: '200' },
+  { title: 'Document Type', align: 'start', key: 'type', width: '170' },
+  { title: 'Status', align: 'start', key: 'published' }
+]
+
+export const items = [
+  {
+    id: 1,
+    shared: '',
+    document: 'Search Engine Optimization Proposal',
+    timeStamp: '2021 Dec 29 2:13PM',
+    type: 'Other',
+    published: true
+  },
+  {
+    id: 2,
+    shared: '',
+    document: 'Freelance Contract',
+    timeStamp: '2022 Dec 19 2:13PM',
+    type: 'Other',
+    published: true
+  },
+  {
+    id: 3,
+    shared: '',
+    document: 'Studio Rental Agreement',
+    timeStamp: '2023 Jan 12 11:33PM',
+    type: 'Other',
+    published: false
+  },
+  {
+    id: 4,
+    shared: '',
+    document: 'Marketing Campaign Quote',
+    timeStamp: '2023 Apr 2 8:33PM',
+    type: 'Other',
+    published: false
+  },
+  {
+    id: 5,
+    shared: '',
+    document: 'Search Engine Optimization Proposal',
+    timeStamp: '2021 Dec 29 2:13PM',
+    type: 'Other',
+    published: true
+  },
+  {
+    id: 6,
+    shared: '',
+    document: 'Freelance Contract',
+    timeStamp: '2022 Dec 19 2:13PM',
+    type: 'Other',
+    published: true
+  },
+  {
+    id: 7,
+    shared: '',
+    document: 'Studio Rental Agreement',
+    timeStamp: '2023 Jan 12 11:33PM',
+    type: 'Other',
+    published: false
+  },
+  {
+    id: 8,
+    shared: '',
+    document: 'Marketing Campaign Quote',
+    timeStamp: '2023 Apr 2 8:33PM',
+    type: 'Other',
+    published: false
+  },
+  {
+    id: 9,
+    shared: '',
+    document: 'Freelance Contract',
+    timeStamp: '2022 Dec 19 2:13PM',
+    type: 'Other',
+    published: true
+  },
+  {
+    id: 10,
+    shared: '',
+    document: 'Studio Rental Agreement',
+    timeStamp: '2023 Jan 12 11:33PM',
+    type: 'Other',
+    published: false
+  },
+  {
+    id: 11,
+    shared: '',
+    document: 'Marketing Campaign Quote',
+    timeStamp: '2023 Apr 2 8:33PM',
+    type: 'Other',
+    published: false
+  }
+]
