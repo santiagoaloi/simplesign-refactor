560901f1c045dda1f16b914aef96a45e7e521122
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 01:53:25 +0200

    refactor(notFound): props and classes

--- src/@core/components/NotFound.vue
+++ src/@core/components/NotFound.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="px-4 py-10 text-center sm:px-6 lg:px-8">
-    <h1 class="block text-7xl font-bold text-gray-800 dark:text-white sm:text-9xl">:(</h1>
-    <h1 class="block text-2xl font-bold text-white"></h1>
-    <p class="mt-3 text-gray-600 dark:text-gray-400">{{ title }}</p>
-    <p class="text-gray-600 dark:text-gray-400">{{ subtitle }}</p>
+  <div class="wrapper">
+    <h1>{{ top }} </h1>
+    <p>{{ title }}</p>
+    <p>{{ subtitle }}</p>
   </div>
 </template>
 <script setup>
 defineProps({
+  top: {
+    type: String,
+    default: ''
+  },
+
   title: {
     type: String,
     default: ''
@@ -18,3 +22,17 @@ defineProps({
   }
 })
 </script>
+
+<style scoped>
+.wrapper { 
+  @apply px-4 text-center sm:px-6 lg:px-8
+}
+
+h1 {
+  @apply block text-7xl font-bold text-gray-800 dark:text-white sm:text-9xl
+}
+
+p {
+  @apply mt-3 text-gray-600 dark:text-gray-400
+}
+</style>
