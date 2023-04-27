06e1386b5ead01f4450464d582f113fcb2ad5eab
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-27 10:22:47 +0200

    chore(templatedrawer): tweaked props

--- src/@core/components/template-drawer/TemplateDrawer.vue
+++ src/@core/components/template-drawer/TemplateDrawer.vue
@@ -1,39 +1,37 @@
 <template>
-  <div>
-    <VNavigationDrawer color="drawer" location="right" order="1" width="390">
-      <template #prepend>
-        <div class="pt-3 px-3">
-          <VBtn block class="mb-4" color="green-button"> Save Changes</VBtn>
-
-          <VTabs v-model="activeTab" class="border-b" color="primary" density="compact" grow>
-            <VTooltip
-              v-for="(tabElement, i) in elements"
-              v-bind="{ ...tooltipDefaults }"
-              :key="i"
-              :text="tabElement.tooltip"
-            >
-              <template #activator="{ props }">
-                <VTab :to="tabElement.to" v-bind="props">
-                  <VIcon :icon="tabElement.icon" />
-                </VTab>
-              </template>
-            </VTooltip>
-          </VTabs>
-        </div>
-      </template>
-
-      <!-- Child routes from Edit-Template will render here -->
+  <VNavigationDrawer color="drawer" location="right" order="1" width="390">
+    <template #prepend>
       <div class="pt-3 px-3">
-        <RouterView v-slot="{ Component }">
-          <template v-if="Component">
-            <VFadeTransition hide-on-leave>
-              <Component :is="Component" />
-            </VFadeTransition>
-          </template>
-        </RouterView>
+        <VBtn block class="mb-4" color="green-button"> Save Changes</VBtn>
+
+        <VTabs v-model="activeTab" class="border-b" color="primary" density="compact" grow>
+          <VTooltip
+            v-for="(tabElement, i) in elements"
+            v-bind="{ ...tooltipDefaults }"
+            :key="i"
+            :text="tabElement.tooltip"
+          >
+            <template #activator="{ props }">
+              <VTab :ripple="false" :to="tabElement.to" rounded="0" v-bind="props">
+                <VIcon :icon="tabElement.icon" />
+              </VTab>
+            </template>
+          </VTooltip>
+        </VTabs>
       </div>
-    </VNavigationDrawer>
-  </div>
+    </template>
+
+    <!-- Child routes from Edit-Template will render here -->
+    <div class="pt-3 px-3">
+      <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+          <VFadeTransition hide-on-leave>
+            <Component :is="Component" />
+          </VFadeTransition>
+        </template>
+      </RouterView>
+    </div>
+  </VNavigationDrawer>
 </template>
 
 <script setup>
