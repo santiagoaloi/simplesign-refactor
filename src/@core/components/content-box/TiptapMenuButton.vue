feabc445d1b52f2ff45fd234c6c40f0aa58ef646
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 01:54:39 +0200

    feat(tiptapmenubutton): add component

---
+++ src/@core/components/content-box/TiptapMenuButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button
+    :class="{ 'is-active': isActive ? isActive() : null }"
+    :title="title"
+    class="menu-item"
+    @click="action"
+  >
+    <VIcon :icon="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+
+    action: {
+      type: Function,
+      required: true
+    },
+
+    isActive: {
+      type: Function,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.menu-item {
+  background: transparent;
+  border: none;
+  border-radius: 0.4rem;
+
+  cursor: pointer;
+  height: 1.75rem;
+  padding: 0.25rem;
+  margin-right: 0.25rem;
+  width: 1.75rem;
+
+  &.is-active,
+  &:hover {
+    color: #0a2be5;
+  }
+}
+</style>
