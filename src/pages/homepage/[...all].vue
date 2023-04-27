70a8539748e31803322702d6ac2754775788e893
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 01:58:15 +0200

    chore(all): use notfound component

--- src/pages/homepage/[...all].vue
+++ src/pages/homepage/[...all].vue
@@ -1,11 +1,9 @@
 <template>
-  <VContainer class="d-flex fill-height overflow-auto justify-center" fluid>
-    <div class="px-4 py-10 text-center sm:px-6 lg:px-8">
-      <h1 class="block text-7xl font-bold text-gray-800 dark:text-white sm:text-9xl">404</h1>
-      <h1 class="block text-2xl font-bold text-white"></h1>
-      <p class="mt-3 text-gray-600 dark:text-gray-400">Oops, something went wrong.</p>
-      <p class="text-gray-600 dark:text-gray-400">Sorry, we couldn't find your page.</p>
-      <div class="mt-5 flex flex-col items-center justify-center gap-2 sm:flex-row sm:gap-3"></div>
-    </div>
-  </VContainer>
+  <div class="d-flex fill-height align-center justify-center">
+    <NotFound
+      subtitle="Sorry, we couldn't find your page."
+      title="Oops, something went wrong."
+      top="404"
+    />
+  </div>
 </template>
