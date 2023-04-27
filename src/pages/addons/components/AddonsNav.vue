9f467cb14244ffd26e571c8c3aa9e93f74da0c8b
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-27 10:21:57 +0200

    chore: lint / format

--- src/pages/addons/components/AddonsNav.vue
+++ src/pages/addons/components/AddonsNav.vue
@@ -4,7 +4,7 @@
       <VIcon icon="$mdiFileDocumentOutline" />
       <div>All add-Ons</div>
     </div>
-    <div class="mt-4 d-flex gap-2 cursor-pointer">
+    <div class="mt-4 flex x gap-2 cursor-pointer">
       <VIcon icon="$mdiMagnify" />
       <div>Enable Add-Ons</div>
     </div>
