c921a73ba20931f3e84104aae7acc93bddabc516
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 07:40:44 +0200

    chore: linting / formatting

--- src/pages/template/Edit-Template.vue
+++ src/pages/template/Edit-Template.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-100">
+  <div class="h-full">
     <!-- 
     For the child routes to work, <router-view/> 
     in TemplateDrawer needs to be a child of App.vue <router-view/> 
@@ -7,10 +7,10 @@
     -->
     <TemplateDrawer />
 
-    <VContainer class="flex h-100">
+    <VContainer class="flex h-full">
       <VCard elevation="8" height="100%" width="100%">
-        <VCardText class="h-100">
-          <div class="flex h-100 flex-col">
+        <VCardText class="h-full">
+          <div class="flex h-full flex-col">
             <div class="flex gap-2 items-center">
               <h4 class="neutral-color">New Document</h4>
               <div class="text-green-500 cursor-pointer">0.00 USD</div>
@@ -26,7 +26,7 @@
             <VDivider class="mt-3 ml-n5 mr-n5" />
 
             <!-- This is where dropped draggable elements render -->
-            <div class="no-scrollbar flex flex-col overflow-auto h-100 mt-4">
+            <div class="no-scrollbar flex flex-col overflow-auto h-full mt-4">
               <DraggableElementsDropzone />
             </div>
           </div>
