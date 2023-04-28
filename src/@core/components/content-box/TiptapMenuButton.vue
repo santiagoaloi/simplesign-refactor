948b0b46d713d1d8532a54db049a362803546ea6
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 06:23:12 +0200

    chore: options to composition API

--- src/@core/components/content-box/TiptapMenuButton.vue
+++ src/@core/components/content-box/TiptapMenuButton.vue
@@ -10,47 +10,26 @@
   </VBtn>
 </template>
 
-<script>
-export default {
-  props: {
-    icon: {
-      type: String,
-      required: true
-    },
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    required: true
+  },
 
-    title: {
-      type: String,
-      required: true
-    },
+  title: {
+    type: String,
+    required: true
+  },
 
-    action: {
-      type: Function,
-      required: true
-    },
+  action: {
+    type: Function,
+    required: true
+  },
 
-    isActive: {
-      type: Function,
-      default: null
-    }
+  isActive: {
+    type: Function,
+    default: null
   }
-}
+})
 </script>
-
-<style lang="scss">
-.menu-item {
-  background: transparent;
-  border: none;
-  border-radius: 0.4rem;
-
-  cursor: pointer;
-  height: 1.75rem;
-  padding: 0.25rem;
-  margin-right: 0.25rem;
-  width: 1.75rem;
-
-  &.is-active,
-  &:hover {
-    color: #0a2be5;
-  }
-}
-</style>
