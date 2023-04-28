77406ad3aae969c35dbfc718b16f13aede0d10cb
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 05:49:04 +0200

    chore(edit-template): flex

--- src/pages/template/Edit-Template.vue
+++ src/pages/template/Edit-Template.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="fill-height">
     <!-- 
     For the child routes to work, <router-view/> 
     in TemplateDrawer needs to be a child of App.vue <router-view/> 
@@ -7,26 +7,29 @@
     -->
     <TemplateDrawer />
 
-    <VContainer>
-      <VCard elevation="8" height="800" min-height="800" width="100%">
-        <VCardText>
-          <div class="d-flex gap-2 align-center">
-            <h4 class="neutral-color">New Document</h4>
-            <div class="text-green-500 cursor-pointer">0.00 USD</div>
-            <VSpacer />
-            <VBtn
-              prepend-icon="$mdiTrashCanOutline"
-              size="small"
-              text="Whipe"
-              @click="store.clearElements()"
-            />
-          </div>
-        </VCardText>
-        <VDivider />
-
-        <!-- This is where dropped draggable elements render -->
+    <VContainer class="d-flex fill-height">
+      <VCard elevation="8" height="100%" width="100%">
         <VCardText class="fill-height">
-          <DraggableElementsDropzone />
+          <div class="d-flex fill-height flex-column">
+            <div class="d-flex gap-2 align-center">
+              <h4 class="neutral-color">New Document</h4>
+              <div class="text-green-500 cursor-pointer">0.00 USD</div>
+              <VSpacer />
+              <VBtn
+                prepend-icon="$mdiTrashCanOutline"
+                size="small"
+                text="Whipe"
+                @click="store.clearElements()"
+              />
+            </div>
+
+            <VDivider class="mt-3 ml-n5 mr-n5" />
+
+            <!-- This is where dropped draggable elements render -->
+            <div class="no-scrollbar flex flex-col overflow-auto fill-height mt-4">
+              <DraggableElementsDropzone />
+            </div>
+          </div>
         </VCardText>
       </VCard>
     </VContainer>
