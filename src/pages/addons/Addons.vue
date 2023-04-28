60bae18b01e4ddeaea7cc8aa018a7b24841ec150
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 06:22:42 +0200

    style: linting / formatting

--- src/pages/addons/Addons.vue
+++ src/pages/addons/Addons.vue
@@ -1,11 +1,11 @@
 <template>
-  <VContainer class="fill-height">
-    <VCard class="fill-height pa-0 rounded-lg border" color="inner-card" elevation="8" width="100%">
-      <VRow class="fill-height" no-gutters>
+  <VContainer class="h-100">
+    <VCard class="h-100 pa-0 rounded-lg border" color="inner-card" elevation="8" width="100%">
+      <VRow class="h-100" no-gutters>
         <VCol class="border-r dark:border-slate-700" cols="3">
           <AddonsNav />
         </VCol>
-        <VCol class="fill-height overflow-auto">
+        <VCol class="h-100 overflow-auto">
           <AddonsList />
         </VCol>
       </VRow>
