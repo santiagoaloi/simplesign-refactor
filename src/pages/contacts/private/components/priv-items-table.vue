1d45837042f3a4f4a622731098e1587e204a680c
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 06:21:40 +0200

    chore: componentized private table

---
+++ src/pages/contacts/private/components/priv-items-table.vue
@@ -0,0 +1,32 @@
+<template>
+  <VDataTable :headers="headers" :items="items" :search="search" hover item-value="name">
+    <template #item.name="{ item }">
+      <div class="flex items-center">
+        <VAvatar size="35">
+          <VImg :src="item.raw.avatar">
+            <template #placeholder>
+              <div class="flex items-center justify-center h-100">
+                <VProgressCircular color="grey-lighten-4" indeterminate></VProgressCircular>
+              </div>
+            </template>
+          </VImg>
+        </VAvatar>
+        <div class="flex flex-col ml-2">
+          <RouterLink to="/">
+            <div class="text-secondary">{{ item.raw.name }}</div>
+          </RouterLink>
+          <small>{{ item.raw.email }}</small>
+        </div>
+      </div>
+    </template>
+  </VDataTable>
+</template>
+<script setup>
+defineProps({
+  search: {
+    type: String,
+    default: ''
+  }
+})
+import { items, headers } from '../data/private-data'
+</script>
