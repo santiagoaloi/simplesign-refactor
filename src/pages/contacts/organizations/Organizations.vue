e4305b9032e84532c661a94642e9d3cbace661c4
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 07:44:22 +0200

    feat(organizations): using flex container

--- src/pages/contacts/organizations/Organizations.vue
+++ src/pages/contacts/organizations/Organizations.vue
@@ -1,24 +1,21 @@
 <template>
-  <VContainer>
-    <VCard class="pa-2 rounded-lg border" elevation="8" width="100%">
-      <VCardText>
-        <div class="flex items-center gap-3">
-          <VTextField
-            v-model="search"
-            color="primary"
-            density="compact"
-            height="20"
-            hide-details
-            placeholder="Search"
-            prepend-inner-icon="$mdiMagnify"
-          />
-          <VSpacer />
-          <VBtn color="green-button" text="Add Organization" />
-        </div>
-        <OrgItemsTable :search="search" />
-      </VCardText>
-    </VCard>
-  </VContainer>
+  <FlexContainerCard>
+    <template #header>
+      <VTextField
+        v-model="search"
+        color="primary"
+        density="compact"
+        height="20"
+        hide-details
+        placeholder="Search"
+        prepend-inner-icon="$mdiMagnify"
+      />
+      <VSpacer />
+      <VBtn color="green-button" text="Add Organization" />
+    </template>
+
+    <OrgItemsTable :search="search" />
+  </FlexContainerCard>
 </template>
 <script setup>
 const search = useDebouncedRef()
