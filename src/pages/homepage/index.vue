124df1611bcd9338f68bfc97045e284d5dbbbf88
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 05:14:44 +0200

    chore(homepage): markraw components

--- src/pages/homepage/index.vue
+++ src/pages/homepage/index.vue
@@ -25,17 +25,17 @@ import LastSignedWidget from './components/LastSignedWidget.vue'
 import MostActiveWidget from './components/MostActiveWidget.vue'
 
 const widgets = reactive([
-  { id: 1, component: ActivityWidget, columns: '6' },
-  { id: 2, component: TasksWidget, columns: '6' },
-  { id: 3, component: RenewalsWidget, columns: '6' },
-  { id: 4, component: RenewalsWidget, columns: '6' },
-  { id: 5, component: ForgottenWidget, columns: '4' },
-  { id: 6, component: LastSignedWidget, columns: '4' },
-  { id: 7, component: MostActiveWidget, columns: '4' },
-  { id: 8, component: UnreadWidget, columns: '4' },
-  { id: 9, component: ExpireWidget, columns: '4' },
-  { id: 10, component: BouncedWidget, columns: '4' },
-  { id: 11, component: TemplateWidget, columns: '4' },
-  { id: 12, component: ContractWidget, columns: '8' }
+  { id: 1, component: markRaw(ActivityWidget), columns: '6' },
+  { id: 2, component: markRaw(TasksWidget), columns: '6' },
+  { id: 3, component: markRaw(RenewalsWidget), columns: '6' },
+  { id: 4, component: markRaw(RenewalsWidget), columns: '6' },
+  { id: 5, component: markRaw(ForgottenWidget), columns: '4' },
+  { id: 6, component: markRaw(LastSignedWidget), columns: '4' },
+  { id: 7, component: markRaw(MostActiveWidget), columns: '4' },
+  { id: 8, component: markRaw(UnreadWidget), columns: '4' },
+  { id: 9, component: markRaw(ExpireWidget), columns: '4' },
+  { id: 10, component: markRaw(BouncedWidget), columns: '4' },
+  { id: 11, component: markRaw(TemplateWidget), columns: '4' },
+  { id: 12, component: markRaw(ContractWidget), columns: '8' }
 ])
 </script>
