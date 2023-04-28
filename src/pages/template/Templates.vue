3ace7f785786126c68b0c6abb002281d817de9a6
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 06:21:03 +0200

    style: linting / formatting

--- src/pages/template/Templates.vue
+++ src/pages/template/Templates.vue
@@ -1,21 +1,21 @@
 <template>
-  <VContainer class="fill-height">
-    <VCard class="fill-height pa-0 rounded-lg border" color="inner-card" elevation="8" width="100%">
-      <VRow class="fill-height" no-gutters>
+  <VContainer class="h-100">
+    <VCard class="h-100 pa-0 rounded-lg border" color="inner-card" elevation="8" width="100%">
+      <VRow class="h-100" no-gutters>
         <VCol class="border-r dark:border-slate-700" cols="3">
           <VCardText class="px-7 pt-7">
             <VBtn block color="green-button" text="Add new template" to="Edit-Template" />
 
-            <div class="mt-7 d-flex gap-1 cursor-pointer">
+            <div class="mt-7 flex gap-1 cursor-pointer">
               <VIcon icon="$mdiFileDocumentOutline" />
               <div>My Templates</div>
             </div>
-            <div class="mt-4 d-flex gap-2 cursor-pointer">
+            <div class="mt-4 flex gap-2 cursor-pointer">
               <VIcon icon="$mdiMagnify" />
               <div>Explore Templates</div>
             </div>
 
-            <div class="mt-7 d-flex align-center">
+            <div class="mt-7 flex items-center">
               <div class="text-overline neutral-color">filters</div>
               <VSpacer />
 
@@ -66,9 +66,9 @@
             </div>
           </VCardText>
         </VCol>
-        <VCol class="fill-height">
-          <div class="d-flex fill-height flex-column">
-            <div class="d-flex align-center gap-1 border-b pb-3">
+        <VCol class="h-100">
+          <div class="flex h-100 flex-col">
+            <div class="flex items-center gap-1 border-b pb-3">
               <VIcon class="mt-3 ml-5" icon="$mdiMagnify" size="18" />
 
               <VTextField
