912d82dac3a8a48a2aab3d691bad0ee215cbebfa
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-28 07:44:06 +0200

    feat(private): using flex container

--- src/pages/contacts/private/Private.vue
+++ src/pages/contacts/private/Private.vue
@@ -1,29 +1,26 @@
 <template>
-  <VContainer>
-    <VCard class="pa-2 rounded-lg border" elevation="8" width="100%">
-      <VCardText>
-        <div class="flex items-center gap-3">
-          <VTextField
-            v-model="search"
-            color="primary"
-            density="compact"
-            height="20"
-            hide-details
-            placeholder="Search"
-            prepend-inner-icon="$mdiMagnify"
-          />
-          <VSpacer />
-          <VBtn
-            v-for="btn in ['Bulk Upload', 'Add Contact']"
-            :key="btn"
-            :text="btn"
-            color="green-button"
-          />
-        </div>
-        <PrivItemsTable :search="search" />
-      </VCardText>
-    </VCard>
-  </VContainer>
+  <FlexContainerCard>
+    <template #header>
+      <VTextField
+        v-model="search"
+        color="primary"
+        density="compact"
+        height="20"
+        hide-details
+        placeholder="Search"
+        prepend-inner-icon="$mdiMagnify"
+      />
+      <VSpacer />
+      <VBtn
+        v-for="btn in ['Bulk Upload', 'Add Contact']"
+        :key="btn"
+        :text="btn"
+        color="green-button"
+      />
+    </template>
+
+    <PrivItemsTable :search="search" />
+  </FlexContainerCard>
 </template>
 <script setup>
 const search = useDebouncedRef()
